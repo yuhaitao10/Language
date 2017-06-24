79a9ca4ab8c1bdd1b393ebf647e412aadd39fe7a
yuhaitao10 <yuhaitao@yahoo.com>
2017-06-24 10:37:00 -0500

    Update list_data_type.py

--- python/scripts/list_data_type.py
+++ python/scripts/list_data_type.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+#index
 family = ['mon', 'dad', 'bro', 'sis', 'dog']		
 family[0]		
 family[-1]		
@@ -8,7 +9,8 @@ family[-1]
 
 print "family list:"
 print family	
-		
+
+#slicing is to get a section of a list
 example=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]		
 example[0:8:2]		
 example[10:0:-2]		
@@ -33,7 +35,8 @@ family[3]='par'
 del family[3]		
 print "family list:"
 print family[:]	
-		
+
+#list methods
 len(family)		
 len('bucky')		
 min(family)		
